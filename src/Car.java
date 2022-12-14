6cbc91263d94768fc4abf2830a9c685c5922704d
Allan22000 <118193446+Allan22000@users.noreply.github.com>
2022-12-14 16:06:36 +0200

    Initial commit

---
+++ src/Car.java
@@ -0,0 +1,44 @@
+
+public class Car {
+String brand;
+String model;
+int amountOfFuel;
+
+public Car() {
+	
+}
+public Car (String brand,String model,int amountOfFuel) {
+	this.brand=brand;
+	this.model=model;
+	this.amountOfFuel=amountOfFuel;
+	printData();
+	
+	
+	
+	
+}
+public void brake() {
+	System.out.println("Car is braking");
+}
+public void accelerate() {
+	System.out.println("Car is accelerating");
+	amountOfFuel--;
+}
+public void refuel(int amount) {
+	System.out.println("Fuel on the tank "+ amountOfFuel);
+	System.out.println("Refuel: " + amount);
+	amountOfFuel+=amount;
+	System.out.println("Fuel in tank after the refuel " + amountOfFuel);
+	
+	
+	
+}
+public void printData() {
+	System.out.println("The brand is "+brand+". ");
+	System.out.println("The model is "+model+".");
+    System.out.println("The amount of fuel"+amountOfFuel+".");
+    
+ 
+	
+}
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car car= new Car("Mercedes","Amg", 11118);
+		car.refuel(1000);
+        Car car1= new Car("Fiat", "500", 50);
+        
+	}
+
+}
